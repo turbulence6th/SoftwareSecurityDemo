1ade706e421fe5759e5ba307c3c0ad5d7e9b7b7a
oguz <turbulence6th@gmail.com>
2019-07-16 00:38:56 +0300

    update

--- src/main/java/tr/com/turbulence6th/security/controller/InjectionController.java
+++ src/main/java/tr/com/turbulence6th/security/controller/InjectionController.java
@@ -1,6 +1,9 @@
 package tr.com.turbulence6th.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 import tr.com.turbulence6th.security.entity.Country;
 import tr.com.turbulence6th.security.entity.User;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpCookie;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,8 +35,8 @@ public class InjectionController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
-        return jdbcTemplate.query("SELECT * FROM users WHERE username = '" + username + "' AND password = '"
+    public String login(@RequestParam String username, @RequestParam String password, HttpServletResponse response) {
+        Optional<User> result = jdbcTemplate.query("SELECT * FROM users WHERE username = '" + username + "' AND password = '"
                 + password + "'", (rs, rowNum) -> {
             User user = new User();
             user.setId(rs.getInt("id"));
@@ -36,9 +44,16 @@ public class InjectionController {
             user.setPassword(rs.getString("password"));
             return user;
         }).stream()
-            .findAny()
-            .map(u -> "Successful login as " + u.getUsername())
-            .orElse("Login failed");
+            .findAny();
+        String body;
+        if (result.isPresent()) {
+            body = "Successful login as " + result.get().getUsername();
+            response.addCookie(new Cookie("token", UUID.randomUUID().toString()));
+        } else {
+            body = "Login failed";
+        }
+
+        return body;
     }
 
     @GetMapping("/country/search")
@@ -49,15 +64,5 @@ public class InjectionController {
             country.setName(rs.getString("name"));
             return country;
         });
-    }
-
-    @GetMapping("/user/read")
-    public List<String> read(@RequestParam String doc) throws IOException {
-        Process process = Runtime.getRuntime().exec("find", doc.split(" "));
-        return lines(process.getInputStream());
-    }
-
-    private List<String> lines(InputStream is) {
-        return new BufferedReader(new InputStreamReader(is)).lines().collect(Collectors.toList());
     }
 }
